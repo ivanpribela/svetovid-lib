a4b1f01fa1435423726d8149eaf0120c34c9a777
Ivan Pribela <ivanpribela@gmail.com>
2014-04-20 17:41:55 +0200

    Add a few demos for the IO library

---
+++ src/org/svetovid/demo/MaxArray.java
@@ -0,0 +1,25 @@
+package org.svetovid.demo;
+
+import org.svetovid.io.Svetovid;
+
+public class MaxArray {
+
+    public static void main(String[] args) {
+
+        // Ask for an array of numbers
+        double[] numbers = Svetovid.in.readDoubleLine("Please enter the numbers: ");
+
+        // Find the maximum
+        double maximum = Double.NEGATIVE_INFINITY;
+        for (double number : numbers) {
+            if (number > maximum) {
+                maximum = number;
+            }
+        }
+
+        // Print the result
+        Svetovid.out.writeln("Maximum number:", maximum);
+        Svetovid.out.writeln(numbers);
+
+    }
+}
