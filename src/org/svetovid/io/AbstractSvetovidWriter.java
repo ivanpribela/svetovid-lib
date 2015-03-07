bcf76097dd32262d52130d7cd7251ded28f16375
Ivan Pribela <ivanpribela@gmail.com>
2015-03-07 12:25:28 +0100

    Add writing of JSON data

--- src/org/svetovid/io/AbstractSvetovidWriter.java
+++ src/org/svetovid/io/AbstractSvetovidWriter.java
@@ -17,6 +17,10 @@
 package org.svetovid.io;
 
 import java.io.IOException;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
 
 import org.svetovid.Svetovid;
 
@@ -731,6 +735,308 @@ public abstract class AbstractSvetovidWriter implements SvetovidWriter {
         }
         for (int i = 0; i < value.length; i++) {
             println(value[i]);
+        }
+    }
+
+    @Override
+    public void printObject(Object value) throws SvetovidIOException {
+        StringBuilder builder = new StringBuilder();
+        appendObject(builder, "", true, value);
+        println(builder.toString());
+    }
+
+    protected final String LINE_SEPARATOR = System
+            .getProperty("line.separator");
+    protected final String INDENT = "  ";
+    protected final String LITERAL_NULL = "null";
+    protected final String LITERAL_TRUE = "true";
+    protected final String LITERAL_FALSE = "false";
+    protected final String STRING_QUOTE = "\"";
+    protected final String BEGIN_OBJECT = "{" + LINE_SEPARATOR;
+    protected final String END_OBJECT = "}";
+    protected final String BEGIN_ARRAY = "[" + LINE_SEPARATOR;
+    protected final String END_ARRAY = "]";
+    protected final String NAME_SEPARATOR = ": ";
+    protected final String VALUE_SEPARATOR = "," + LINE_SEPARATOR;
+
+    protected void appendObject(StringBuilder builder, String prefix,
+            boolean printPrefix, Object value) {
+        if (value == null) {
+            if (printPrefix) {
+                builder.append(prefix);
+            }
+            builder.append(LITERAL_NULL);
+        } else if (value instanceof Boolean) {
+            if ((Boolean) value) {
+                if (printPrefix) {
+                    builder.append(prefix);
+                }
+                builder.append(LITERAL_TRUE);
+            } else {
+                if (printPrefix) {
+                    builder.append(prefix);
+                }
+                builder.append(LITERAL_FALSE);
+            }
+        } else if (value instanceof Number) {
+            if (printPrefix) {
+                builder.append(prefix);
+            }
+            builder.append(((Number) value).toString());
+        } else if (value instanceof String) {
+            if (printPrefix) {
+                builder.append(prefix);
+            }
+            builder.append(STRING_QUOTE);
+            String string = (String) value;
+            for (int i = 0; i < string.length(); i++) {
+                char ch = string.charAt(i);
+                switch (ch) {
+                case '\"':
+                    builder.append("\\\"");
+                    break;
+                case '\\':
+                    builder.append("\\\\");
+                    break;
+                case '\b':
+                    builder.append("\\b");
+                    break;
+                case '\f':
+                    builder.append("\\f");
+                    break;
+                case '\n':
+                    builder.append("\\n");
+                    break;
+                case '\r':
+                    builder.append("\\r");
+                    break;
+                case '\t':
+                    builder.append("\\t");
+                    break;
+                default:
+                    if (ch < 32) {
+                        builder.append("\\u00");
+                        String escape = Integer.toHexString(ch);
+                        if (escape.length() == 1) {
+                            builder.append('0');
+                        }
+                        builder.append(escape);
+                    } else {
+                        builder.append(ch);
+                    }
+                }
+            }
+            builder.append(STRING_QUOTE);
+        } else if (value instanceof Iterable) {
+            if (printPrefix) {
+                builder.append(prefix);
+            }
+            builder.append(BEGIN_ARRAY);
+            Iterator<?> iterator = ((List<?>) value).iterator();
+            if (iterator.hasNext()) {
+                Object object = iterator.next();
+                appendObject(builder, prefix + INDENT, true, object);
+                while (iterator.hasNext()) {
+                    object = iterator.next();
+                    builder.append(VALUE_SEPARATOR);
+                    appendObject(builder, prefix + INDENT, true, object);
+                }
+            }
+            builder.append(LINE_SEPARATOR);
+            builder.append(prefix);
+            builder.append(END_ARRAY);
+        } else if (value instanceof boolean[]) {
+            if (printPrefix) {
+                builder.append(prefix);
+            }
+            builder.append(BEGIN_ARRAY);
+            boolean[] array = (boolean[]) value;
+            if (array.length > 0) {
+                Object object = array[0];
+                appendObject(builder, prefix + INDENT, true, object);
+                for (int i = 1; i < array.length; i++) {
+                    object = array[i];
+                    builder.append(VALUE_SEPARATOR);
+                    appendObject(builder, prefix + INDENT, true, object);
+                }
+            }
+            builder.append(LINE_SEPARATOR);
+            builder.append(prefix);
+            builder.append(END_ARRAY);
+        } else if (value instanceof byte[]) {
+            if (printPrefix) {
+                builder.append(prefix);
+            }
+            builder.append(BEGIN_ARRAY);
+            byte[] array = (byte[]) value;
+            if (array.length > 0) {
+                Object object = array[0];
+                appendObject(builder, prefix + INDENT, true, object);
+                for (int i = 1; i < array.length; i++) {
+                    object = array[i];
+                    builder.append(VALUE_SEPARATOR);
+                    appendObject(builder, prefix + INDENT, true, object);
+                }
+            }
+            builder.append(LINE_SEPARATOR);
+            builder.append(prefix);
+            builder.append(END_ARRAY);
+        } else if (value instanceof short[]) {
+            if (printPrefix) {
+                builder.append(prefix);
+            }
+            builder.append(BEGIN_ARRAY);
+            short[] array = (short[]) value;
+            if (array.length > 0) {
+                Object object = array[0];
+                appendObject(builder, prefix + INDENT, true, object);
+                for (int i = 1; i < array.length; i++) {
+                    object = array[i];
+                    builder.append(VALUE_SEPARATOR);
+                    appendObject(builder, prefix + INDENT, true, object);
+                }
+            }
+            builder.append(LINE_SEPARATOR);
+            builder.append(prefix);
+            builder.append(END_ARRAY);
+        } else if (value instanceof int[]) {
+            if (printPrefix) {
+                builder.append(prefix);
+            }
+            builder.append(BEGIN_ARRAY);
+            int[] array = (int[]) value;
+            if (array.length > 0) {
+                Object object = array[0];
+                appendObject(builder, prefix + INDENT, true, object);
+                for (int i = 1; i < array.length; i++) {
+                    object = array[i];
+                    builder.append(VALUE_SEPARATOR);
+                    appendObject(builder, prefix + INDENT, true, object);
+                }
+            }
+            builder.append(LINE_SEPARATOR);
+            builder.append(prefix);
+            builder.append(END_ARRAY);
+        } else if (value instanceof long[]) {
+            if (printPrefix) {
+                builder.append(prefix);
+            }
+            builder.append(BEGIN_ARRAY);
+            long[] array = (long[]) value;
+            if (array.length > 0) {
+                Object object = array[0];
+                appendObject(builder, prefix + INDENT, true, object);
+                for (int i = 1; i < array.length; i++) {
+                    object = array[i];
+                    builder.append(VALUE_SEPARATOR);
+                    appendObject(builder, prefix + INDENT, true, object);
+                }
+            }
+            builder.append(LINE_SEPARATOR);
+            builder.append(prefix);
+            builder.append(END_ARRAY);
+        } else if (value instanceof float[]) {
+            if (printPrefix) {
+                builder.append(prefix);
+            }
+            builder.append(BEGIN_ARRAY);
+            float[] array = (float[]) value;
+            if (array.length > 0) {
+                Object object = array[0];
+                appendObject(builder, prefix + INDENT, true, object);
+                for (int i = 1; i < array.length; i++) {
+                    object = array[i];
+                    builder.append(VALUE_SEPARATOR);
+                    appendObject(builder, prefix + INDENT, true, object);
+                }
+            }
+            builder.append(LINE_SEPARATOR);
+            builder.append(prefix);
+            builder.append(END_ARRAY);
+        } else if (value instanceof double[]) {
+            if (printPrefix) {
+                builder.append(prefix);
+            }
+            builder.append(BEGIN_ARRAY);
+            double[] array = (double[]) value;
+            if (array.length > 0) {
+                Object object = array[0];
+                appendObject(builder, prefix + INDENT, true, object);
+                for (int i = 1; i < array.length; i++) {
+                    object = array[i];
+                    builder.append(VALUE_SEPARATOR);
+                    appendObject(builder, prefix + INDENT, true, object);
+                }
+            }
+            builder.append(LINE_SEPARATOR);
+            builder.append(prefix);
+            builder.append(END_ARRAY);
+        } else if (value instanceof char[]) {
+            if (printPrefix) {
+                builder.append(prefix);
+            }
+            builder.append(BEGIN_ARRAY);
+            char[] array = (char[]) value;
+            if (array.length > 0) {
+                Object object = array[0];
+                appendObject(builder, prefix + INDENT, true, object);
+                for (int i = 1; i < array.length; i++) {
+                    object = array[i];
+                    builder.append(VALUE_SEPARATOR);
+                    appendObject(builder, prefix + INDENT, true, object);
+                }
+            }
+            builder.append(LINE_SEPARATOR);
+            builder.append(prefix);
+            builder.append(END_ARRAY);
+        } else if (value instanceof Object[]) {
+            if (printPrefix) {
+                builder.append(prefix);
+            }
+            builder.append(BEGIN_ARRAY);
+            Object[] array = (Object[]) value;
+            if (array.length > 0) {
+                Object object = array[0];
+                appendObject(builder, prefix + INDENT, true, object);
+                for (int i = 1; i < array.length; i++) {
+                    object = array[i];
+                    builder.append(VALUE_SEPARATOR);
+                    appendObject(builder, prefix + INDENT, true, object);
+                }
+            }
+            builder.append(LINE_SEPARATOR);
+            builder.append(prefix);
+            builder.append(END_ARRAY);
+        } else if (value instanceof Map) {
+            if (printPrefix) {
+                builder.append(prefix);
+            }
+            builder.append(BEGIN_OBJECT);
+            @SuppressWarnings("unchecked")
+            Map<Object, Object> map = (Map<Object, Object>) value;
+            Iterator<Entry<Object, Object>> iterator = map.entrySet().iterator();
+            if (iterator.hasNext()) {
+                Entry<Object, Object> object = iterator.next();
+                String name = object.getKey().toString();
+                Object val = object.getValue();
+                appendObject(builder, prefix + INDENT, true, name);
+                builder.append(NAME_SEPARATOR);
+                appendObject(builder, prefix + INDENT, false, val);
+                while (iterator.hasNext()) {
+                    object = iterator.next();
+                    name = object.getKey().toString();
+                    val = object.getValue();
+                    builder.append(VALUE_SEPARATOR);
+                    appendObject(builder, prefix + INDENT, true, name);
+                    builder.append(NAME_SEPARATOR);
+                    appendObject(builder, prefix + INDENT, false, val);
+                }
+            }
+            builder.append(LINE_SEPARATOR);
+            builder.append(prefix);
+            builder.append(END_OBJECT);
+        } else {
+            appendObject(builder, prefix, printPrefix, value.toString());
         }
     }
 }
