98e459e4e4fabd130cbdc4baa60ba95644227c97
Ivan Pribela <ivanpribela@gmail.com>
2015-04-07 12:27:20 +0200

    WeatherConditions demo, data display

--- src/org/svetovid/demo/WeatherConditions.java
+++ src/org/svetovid/demo/WeatherConditions.java
@@ -14,7 +14,18 @@ public class WeatherConditions {
         String uri = "http://api.openweathermap.org/data/2.5/weather?"
                 + "units=metric&mode.json&q=" + location;
         Object data = Svetovid.in(uri).readObject();
-        Svetovid.err.printObject(data);
+
+        // Extract the data
+        String weather =     JsonHelper.getString(data, "weather[0].main");
+        Number temperature = JsonHelper.getNumber(data, "main.temp");
+        Number humidity =    JsonHelper.getNumber(data, "main.humidity");
+        Number wind =        JsonHelper.getNumber(data, "wind.speed");
+
+        // Print the weather information
+        Svetovid.out.println("    Weather:", weather);
+        Svetovid.out.println("Temperature:", temperature + "°C");
+        Svetovid.out.println("   Humidity:", humidity + "%");
+        Svetovid.out.println("       Wind:", wind + " m/s");
 
     }
 }
