f70aa2b79d03a2357af4c89798a0dbb2dec7f920
Doni Pracner <quinnuendo@gmail.com>
2020-09-06 20:16:15 +0200

    Uninstall script, linux, more messages and warrnings

--- installers/uninstall.sh
+++ installers/uninstall.sh
@@ -14,7 +14,7 @@ fi
 # Check if sed is available
 sed --version >/dev/null 2>&1
 if [ $? -ne 0 ]; then
-  echo "Sed is not available"
+  echo "Sed is not available, please remove the CLASSPATH entries manually from $config"
   exit
 fi
 
@@ -27,10 +27,18 @@ else
     echo "Problem with removing the file $folder/$file"
 fi
 
+echo "This will now remove the CLASSPATH entry from $config entirely."
+echo "If you have additional modifications to this entry, they will be removed."
+echo "If you want to keep those, stop this process and change CLASSPATH manually."
+read -p "Continue? [Yn]" input
 
+if [ -n $input ] && [ $input != "Y" ]; then
+    exit
+fi
 
 # Unset the class path
 if grep -q "$variable" "$config" 2>/dev/null; then
   sed -i "/$variable=.*/d" "$config"
 fi
 
+echo "CLASSPATH entry removed from $config"
