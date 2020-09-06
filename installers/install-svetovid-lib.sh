ac3d2790818049a327309f6e075227bd17aa5599
Doni Pracner <quinnuendo@gmail.com>
2020-09-06 19:48:34 +0200

    Installer script, more messages to the user; try to continue without sed

--- installers/install-svetovid-lib.sh
+++ installers/install-svetovid-lib.sh
@@ -8,28 +8,29 @@ variable='CLASSPATH'
 
 # Check if administrator
 if [ $(id -u) -ne 0 ]; then
-  echo "Please run as administrator"
+  echo "Please run as administrator/super user"
   exit
 fi
 
 # Check if curl is available
 curl -V >/dev/null 2>&1
 if [ $? -ne 0 ]; then
-  echo "Curl is not available"
+  echo "Curl is not available, can not download svetovid-lib.jar"
   exit
 fi
 
 # Check if sed is available
 sed --version >/dev/null 2>&1
 if [ $? -ne 0 ]; then
-  echo "Sed is not available"
-  exit
+  echo "Sed is not available, CLASSPATH will be just appended. This may cause problems if installed multiple times."
+  nosed=1
 fi
 
 # Create the folder
 mkdir -p "$folder"
 
 # Download the library
+echo "Downloading library from $url"
 curl -fsL "$url" -o "$folder/$file"
 if [ $? -ne 0 ]; then
   echo "Failed to download Svetovid library"
@@ -38,9 +39,10 @@ fi
 
 # Set the class path
 value="$folder/$file:."
-if grep -q "$variable" "$config" 2>/dev/null; then
+if [ -z $nosed ] && grep -q "$variable" "$config" 2>/dev/null; then
   sed -i "s|$variable=.*|$variable=\"$value\"|" "$config"
 else
   echo "$variable=\"$value\"" >> "$config"
 fi
 
+echo "CLASSPATH set to use svetovid-lib; please re-login for the changes to be applied"
